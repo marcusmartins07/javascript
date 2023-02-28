3da985bf28baa03ed78094261ae2b13391238b11
Mark-py <38411292+marcusmartins07@users.noreply.github.com>
2023-02-27 22:27:16 -0300

    Update filter.js

--- aula065/filter.js
+++ aula065/filter.js
@@ -30,11 +30,17 @@ console.log(numerosFiltrados);
 // Retorne as pessoas com de 50 anos
 // Retorne as pessoas cujo nome termina com a 
 
-/* const pessoas = [
-    { nome = 'Luiz', idade: 62 },
-    { nome = 'Maria', idade: 23 },
-    { nome = 'Eduardo', idade: 55 },
-    { nome = 'Leticia', idade: 19 },
-    { nome = 'Rosana', idade: 32 },
-    { nome = 'Wallace', idade: 47 },
-]; */
+ const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Leticia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]; 
+const pessoaComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5); 
+console.log(pessoaComNomeGrande);
+const pessoasVelhas = pessoas.filter(obj => obj.idade > 50);
+console.log(pessoasVelhas);
+const nomeTerminaComA = pessoas.filter(obj => obj.nome.toLowerCase(). endsWith('a'));
+console.log(nomeTerminaComA);
