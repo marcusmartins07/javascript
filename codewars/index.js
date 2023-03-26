4a256ebc811a7756abdc4bfa26f39b384df4b763
marcusmartins07 <marvyn0710@gmail.com>
2023-03-26 10:09:52 -0300

    Create codewars

--- codewars/index.js
+++ codewars/index.js
@@ -1,10 +1,39 @@
-
-
-function criaCarro (marca, modelo, ano) {
-    return marca;
-    return modelo;
-    return ano;
+class CriaCarro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.motor = false;
+    }
+    
+    ligar(){
+        if (this.motor){
+            console.log(`${this.modelo} já ligado`);
+            return;
+        }
+        console.log(`${this.modelo} esta dando partida`);
+        this.motor = true;
+    }
+        
+    desligar(){
+        if (!this.motor){
+            console.log(`${this.modelo} já desligado`);
+            return;
+        }
+        console.log(`${this.modelo} esta desligando o motor`);
+        this.motor = false;
+    }
 }
 
-const carroNovo = criaCarro('Ford', 'Ka', 2001);
-console.log(carroNovo);
+
+
+const c1 = new CriaCarro('Ford', 'Ka', '2001');
+console.log(c1);
+c1.ligar();
+console.log(c1);
+c1.ligar();
+console.log(c1);
+c1.desligar();
+console.log(c1);
+c1.desligar();
+console.log(c1);
