33b26d2f06d338ef6597a596fd93d98ba360e2f5
marcusmartins07 <marvyn0710@gmail.com>
2023-04-23 15:12:35 -0300

    #

---
+++ aula091/main.js
@@ -0,0 +1,11 @@
+const nome = 'luiz';
+const obj = {nome};
+const novoObj = { ...obj};
+console.log(novoObj);
+
+class Pessoa {
+  constructor (nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+}
